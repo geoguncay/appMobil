beda2ec65ed75bae5f076e366b0d7b2c51354424
LAURO GEOVANNY GUNCAY TIGRE <lgguncay@utpl.edu.ec>
2024-01-14 01:52:48 -0500

    added api, models, ui

--- api_tarea/lib/main.dart
+++ api_tarea/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:api_tarea/Screen/home_screen.dart';
+import 'package:api_tarea/api/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'ui/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,14 +9,19 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  static const String _title = 'Tarea API';
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+    return Provider<ApiService>(
+      create: (_) => ApiService(),
+      child: MaterialApp(
+        title: 'Rick and Morty API',
+        theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+        debugShowCheckedModeBanner: false,
+        home: const HomeScreen(),
+      ),
     );
   }
 }
